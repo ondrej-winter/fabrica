eb04065cad30bcfb9550a5aeebd9ad92e42da3fe
Ondřej Winter <ondrej.winter@gmail.com>
2026-08-05 12:15:13 +0200

    refactor(developer-workflow)!: remove PrepareCommitMessageRun use-case and its exports

--- src/fabrica/features/developer_workflow/application/use_cases/__init__.py
+++ src/fabrica/features/developer_workflow/application/use_cases/__init__.py
@@ -1,17 +1,13 @@
 """Application use cases for developer workflow orchestration."""
 
+from fabrica.features.developer_workflow.application.dtos import DEFAULT_COMMIT_MESSAGE_SKILL_ID
 from fabrica.features.developer_workflow.application.use_cases.generate_commit_message import (
     GenerateCommitMessage,
     GenerateCommitMessageError,
-)
-from fabrica.features.developer_workflow.application.use_cases.prepare_commit_message_run import (
-    DEFAULT_COMMIT_MESSAGE_SKILL_ID,
-    PrepareCommitMessageRun,
 )
 
 __all__ = [
     "DEFAULT_COMMIT_MESSAGE_SKILL_ID",
     "GenerateCommitMessage",
     "GenerateCommitMessageError",
-    "PrepareCommitMessageRun",
 ]
